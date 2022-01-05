1420d8b4ff2d59f7d625eecf22e7f1d09ca854e1
maccmspro-s3 <85347809+maccmspro@users.noreply.github.com>
2022-01-05 22:25:06 +0800

    升级到2022.1.2

--- application/extra/version.php
+++ application/extra/version.php
@@ -3,7 +3,6 @@ return array (
     'name' => 'MacCMS Pro内容管理系统',
     'copyright' => 'MacCMS Pro',
     'url' => '//github.com/maccmspro',
-    'code' => '2022.1.1',
+    'code' => '2022.1.2',
     'license' => '免费版',
 );
-?>
